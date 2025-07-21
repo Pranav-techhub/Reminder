db06980244983c1b259913156c3f4d6c5b4be596
Pranav-techhub <pranav.24sk@gmail.com>
2025-07-21 23:05:07 +0530

    Delete Reminder App directory

--- Reminder App/model/reminder_model.py
+++
@@ -1,8 +0,0 @@
-# model/reminder_model.py
-
-class Reminder:
-    def __init__(self, reminder_id, text):
-        self.id = reminder_id
-        self.text = text
-        self.is_done = False
-            
